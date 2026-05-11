93f7dc723883a6f0b6afbbc66db61f9899735a96
Andres <roanbupa@gmail.com>
2026-05-11 16:08:40 -0300

    feat: conectar express con postgresql usando pg

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const authorsRouter = require("./routes/authors");
 const postsRouter = require("./routes/posts");
+const pool = require("./db/config");
 
 const app = express();
 const PORT = 3000;
@@ -26,6 +27,15 @@ app.use((req, res) => {
     error: "Ruta no encontrada",
   });
 });
+
+pool
+  .connect()
+  .then(() => {
+    console.log("Conectado a PostgreSQL");
+  })
+  .catch((err) => {
+    console.error("Error de conexión:", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
