b0f64ec2b4fc02d94dd3b7fec9c89227b1f5e4d2
Andres <roanbupa@gmail.com>
2026-05-16 17:11:47 -0300

    Fix Railway port

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./openapi.yaml");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
